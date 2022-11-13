9b56a4693e22b3031c609e3b79a273f12b95788d
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-11-13 12:55:49 +0500

    solving symmetric tree task - will think out better solution later

--- src/main/java/easy/symmetric_tree/Solution.java
+++ src/main/java/easy/symmetric_tree/Solution.java
@@ -3,8 +3,32 @@ package easy.symmetric_tree;
 
 import base_classes.TreeNode;
 
+import java.util.Stack;
+
 public class Solution {
-    public boolean isSymmetric(TreeNode roof) {
-        return false;
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        TreeNode left = root;
+        TreeNode right = root;
+        Stack<TreeNode> leftStack = new Stack<>();
+        Stack<TreeNode> rightStack = new Stack<>();
+        while (!leftStack.isEmpty() && !rightStack.isEmpty() || left != null && right != null) {
+            if (left != null && right != null) {
+                if (left.val != right.val) return false;
+                leftStack.push(left);
+                rightStack.push(right);
+                left = left.left;
+                right = right.right;
+            } else if (left == null && right == null){
+                left = leftStack.pop().right;
+                right = rightStack.pop().left;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
     }
 }
